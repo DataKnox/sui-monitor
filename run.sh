af52f8d03757a7862647e07e5efcda4a1eb9e982
Knox Hutchinson <knox@knoxsdata.com>
2023-03-17 15:13:18 -0500

    fix sh again

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export TABLE_NAME="testDB"
-export DATABASE_NAME="testtbl"
-python3 collect.py
+export TABLE_NAME="suimon"
+export DATABASE_NAME="testDB"
+python3 /home/sui/sui-monitor/collect.py
