267440dd25907b2ddcb21377d8302a4b9503ae0d
Knox Hutchinson <knox@knoxsdata.com>
2023-05-01 14:33:55 -0500

    unstakoor

--- staketask.py
+++ staketask.py
@@ -9,7 +9,10 @@ with open('/home/sui/gas.txt', 'r') as f:
     second_read = f.readlines()
     for line in second_read:
         print(f"Line: {line}")
-        merging_obj = line.strip()
-        merging_obj = merging_obj.split(' ')[0]
-        print("merging:\n")
-        print(merging_obj)
+        stake_obj = line.strip()
+        stake_obj_id = stake_obj.split(' ')[0]
+        print("stake obj id:")
+        print(stake_obj_id)
+        os.popen(
+            f'/home/sui/sui/target/debug/sui client call --package 0x3 --module sui_system --function request_withdraw_stake --args 0x5 {stake_obj_id} --gas-budget 20000000')
+        time.sleep(5)
