5c5954f9944ecdcbdaed57fcd6a01012fcce5337
Knox Hutchinson <knox@knoxsdata.com>
2023-05-01 14:36:05 -0500

    filename got me

--- staketask.py
+++ staketask.py
@@ -5,7 +5,7 @@ to_file = os.popen(
     '/home/sui/sui/target/debug/sui client objects | grep StakedSui > /home/sui/stake.txt')
 
 time.sleep(5)
-with open('/home/sui/gas.txt', 'r') as f:
+with open('/home/sui/stake.txt', 'r') as f:
     second_read = f.readlines()
     for line in second_read:
         print(f"Line: {line}")
