743b6b315c116642a60b61f2ce18af2c835da71d
Knox Hutchinson <knox@knoxsdata.com>
2023-10-11 21:05:07 -0500

    spacing

--- staketask.py
+++ staketask.py
@@ -26,6 +26,8 @@ with open('/home/sui/stake.txt', 'r') as f:
         stake_obj_eval = stake_obj.split(' ')[2]
         print(stake_obj_eval)
         if stake_obj_eval == 'objectId':
+            stake_obj.replace('   ', ' ')
+            stake_obj.replace('  ', ' ')
             print('0 ' + stake_obj.split(' ')[0])
             print('4 ' + stake_obj.split(' ')[4])
             stake_obj_id = stake_obj.split(' ')[4]
